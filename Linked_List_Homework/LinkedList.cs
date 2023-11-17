752e943a61a4e4d3f9a00bf363bbd9c14469072f
TokaAjaj <149011386+TokaAjaj@users.noreply.github.com>
2023-11-17 22:27:32 +0300

    Update LinkedList.cs with solving homewrok

--- Linked_List_Homework/LinkedList.cs
+++ Linked_List_Homework/LinkedList.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Linked_List_Homework
 {
@@ -11,33 +11,100 @@ namespace Linked_List_Homework
             Node move = First;
             while (move != null)
             {
-                Console.Write(move.Data+"\t");
+                Console.Write(move.Data + "\t");
                 move = move.Next;
             }
             Console.WriteLine();
         }
 
-        // methods
         public void Add(int val)
         {
-            // TODO: add item to the end of the list
-            // consider when the list is empty
+            Node newNode = new Node(val);
 
-            // create a new node with the given data
+            if (First == null)
+            {
+                First = newNode;
+            }
+            else
+            {
+                Node current = First;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
         }
+
         public void RemoveKey(int key)
         {
-            // TODO: search for the key and remove it from the list
-            // consider when the key does not exist and when the list is empty
+            if (First == null)
+            {
+                Console.WriteLine("Sorry List is empty. Nothing to remove.");
+                return;
+            }
+
+            if (First.Data == key)
+            {
+                First = First.Next;
+                return;
+            }
+
+            Node current = First;
+            Node previous = null;
+
+            while (current != null && current.Data != key)
+            {
+                previous = current;
+                current = current.Next;
+            }
+
+            if (current == null)
+            {
+                Console.WriteLine("Sorry Key not found in the list.");
+                return;
+            }
+
+            previous.Next = current.Next;
         }
+
         public void Merge(LinkedList other_list)
         {
-            // TODO: merge this list with the other list
+            if (other_list.First == null)
+            {
+                Console.WriteLine("Sorry The other list is empty. Nothing to merge.");
+                return;
+            }
+
+            if (First == null)
+            {
+                First = other_list.First;
+                return;
+            }
+
+            Node current = First;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = other_list.First;
         }
+
         public void Reverse()
         {
-            // TODO: revers the nodes of this list
-            // initialize three pointers: prev, curr, and next
+            Node prev = null;
+            Node current = First;
+
+            while (current != null)
+            {
+                Node next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+
+            First = prev;
         }
     }
 }
